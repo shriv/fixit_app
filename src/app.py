29a8f0ad228e74ec17734ea5c0c8c76b6dc3e10c
Shrividya Ravi <pentheseleia+bitbucket@gmail.com>
2020-02-09 19:00:32 +1300

    connected to live DB and only viewing traps still to be fixed

--- src/app.py
+++ src/app.py
@@ -23,7 +23,7 @@ import numpy as np
 
 
 def get_data():
-    fixit_df = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vTw6mmYBzkbfIj4bjDHgfg0cebumhMEzrLkC290T0Zpp1t4o7Z8I4ep6MdwnwdvQsEydHaVjmYVmWsp/pub?output=tsv', sep='\t')
+    fixit_df = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSwfyvQHwOopLW2pPDLm3wMZnaNTBlAleWZMeSf3uZVNUtJFoclY1ocYMk_6ywJts_OfiZHG71ShmsJ/pub?output=tsv', sep='\t')
 
     fixit_df['trap_code'] = (fixit_df['Trap Number']
                               .astype(str)
@@ -43,40 +43,54 @@ def get_data():
 
     return data_clean
 
-def clean_data():
-    data_clean = get_data()
-    data_clean = data_clean[data_clean['date'] >= '2019-12-01']
-    return data_clean
+
+############################
+## MAIN APP FUNCTIONALITY ##
+############################
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    data_clean = clean_data()
+    # Get data from Google sheets
+    data_clean = get_data()
+    data_clean = data_clean[data_clean['Date Trap Fixed'].isnull()]
+
     # Pull out relevant columns for leaflet map
     vname = list(data_clean["Trap Number"])
     lat = list(data_clean["lat"])
     lon = list(data_clean["lon"])
     elev = list(data_clean["date"].dt.strftime('%Y-%m-%d'))
-    status = list(data_clean["Description From Trapper"])
+    issue = list(data_clean["Description From Trapper"])
 
+    # make map
     map = folium.Map(location=[-41.3, 174.9], zoom_start=12, tiles="OpenStreetMap")
 
-    fgv = folium.FeatureGroup(name="Traps")
+    # Traps layer
+    fgv = folium.FeatureGroup(name="Fixit To Do")
 
-    for lt, ln, el, st, vnm in zip(lat, lon, elev, status, vname):
-        fgv.add_child(folium.CircleMarker(location=[lt, ln], radius = 8, popup= folium.Popup("<b> Trap: </b>" + vnm + "<br> " +
-                "<b> Reported: </b>" + el + "<br> ", max_width=450),
-        fill_color='red', color = 'red', fill=True, fill_opacity=0.7))
-
+    # Add points
+    for lt, ln, el, iss, vnm in zip(lat, lon, elev, issue, vname):
+        fgv.add_child(folium.CircleMarker(location=[lt, ln],
+                                          radius = 8,
+                                          popup = folium.Popup("<b> Trap: </b>" +
+                                                               vnm + "<br> " +
+                                                               "<b> Reported: </b>" +
+                                                               el + "<br> " +
+                                                               "<b> Issue: </b>" +
+                                                               iss + "<br> ",
+                                                               max_width=450),
+                                          fill_color='red',
+                                          color = 'red',
+                                          fill=True,
+                                          fill_opacity=0.7))
     map.add_child(fgv)
     map.add_child(folium.LayerControl())
+
+    
+
     return map._repr_html_()
-#
-# def index():
-#     start_coords = (-41.3, 174.77)
-#     folium_map = folium.Map(location=start_coords, zoom_start=14)
-#     return folium_map._repr_html_()
+
 
 
 if __name__ == '__main__':
